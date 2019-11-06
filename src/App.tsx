dea3ccd7034038836974721138b6d85ac4d6d7d3
Ben Lardie <benlardie@gmail.com>
2019-11-05 23:33:27 -0500

    added fetch but getting error

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,29 @@
-import React from 'react'
+import React, { useEffect, useContext } from 'react'
 import { Store } from './Store'
 
+
 export default function App():JSX.Element {
-  const store = React.useContext(Store)
+  const [state, dispatch] = useContext(Store)
+
+  useEffect(() => {
+    state.episodes === 0 && fetchDataAction()
+  })
+
+  const fetchDataAction = async () => {
+    const URL = 'https://api.tvmaze.com/singlesearch/shows?q=the-office&embed=episodes'
+    const data = await fetch(URL)
+    const dataJSON = await data.json()
+    return dispatch({
+      type: 'FETCH_DATA',
+      payload: dataJSON._embedded.episodes
+    })
+  }
+  console.log(state)
+
   return (
     <>
       <h1>Rick and Morty</h1>
       <p>Pick your favourite episode</p>
-      {console.log(store)}
     </>
   )
 }
